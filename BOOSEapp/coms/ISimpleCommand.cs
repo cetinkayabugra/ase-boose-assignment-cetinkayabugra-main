bc0e193b40d0758dae9ccc8ae1a0766fa9c2cf68
Bugra Cetinkaya <cetinkayabugra23@gmail.com>
2026-01-12 05:38:28 +0000

    XML Summarise

--- BOOSEapp/coms/ISimpleCommand.cs
+++ BOOSEapp/coms/ISimpleCommand.cs
@@ -9,16 +9,24 @@
 namespace BOOSEapp
 {
     /// <summary>
-    /// Simple command interface
+    /// Represents a basic executable command within the BOOSE interpreter.
     /// </summary>
+    /// <remarks>
+    /// All commands parsed from a BOOSE program must implement this
+    /// interface in order to be executed by the ProgramExecutor.
+    /// </remarks>
     public interface ISimpleCommand
     {
         void Execute();
     }
 
     /// <summary>
-    /// Control flow command interface
+    /// Represents a control flow command such as loops or conditionals.
     /// </summary>
+    /// <remarks>
+    /// Control flow commands require access to the ProgramExecutor
+    /// to manipulate execution order (e.g. while loops).
+    /// </remarks>
     public interface IControlFlowCommand : ISimpleCommand
     {
         void SetExecutor(ProgramExecutor executor);
